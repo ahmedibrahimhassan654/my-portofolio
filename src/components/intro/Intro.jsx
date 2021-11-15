86ba59cfc2a30695ee617331a02a622892eebc4b
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-11-15 20:52:42 +0200

    designe and style intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,28 @@
-import React from "react";
-
+import "./intro.scss";
+import { useEffect, useRef } from "react";
 const Intro = () => {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imagecontainer">
+          <img src="assets/myimg.png" alt=" " />
+        </div>
+      </div>
+
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Ahmed Ibrahim</h1>
+          <h3>
+            Freelance <span></span>
+          </h3>
+        </div>
+        <a href="#portofolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Intro;
--- src/components/menu/Menue.jsx
+++ src/components/menu/Menue.jsx
@@ -3,20 +3,20 @@ const Menue = ({ menuOpen, setMenuOpen }) => {
   return (
     <div className={"menue " + (menuOpen && "active")}>
       <ul>
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <a href="#intro"> Home </a>
         </li>
 
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <a href="#portofolio"> Portofolio </a>
         </li>
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <a href="#works"> Works </a>
         </li>
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <a href="#testimonial"> Testimonials </a>
         </li>
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <a href="#contact"> Contact </a>
         </li>
       </ul>
