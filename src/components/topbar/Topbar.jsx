a79141def2ada948eb7327fabab79cc7dbb0b057
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-11-15 11:00:12 +0200

    Using Material-UI Icons with React

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,24 +1,35 @@
 import "./topbar.scss";
 import { Person, Mail } from "@material-ui/icons";
+import HomeIcon from "@mui/icons-material/Home";
 const Topbar = () => {
   return (
-    <div className="topbar">
+    <div className="topbar active">
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
             Genius .
           </a>
           <div className="itemContainer">
-            {/* <Person className="icon" /> */}
-            <span>+44 924 12 74</span>
+            <Person className="icon" />
+            <span>002 01065619208 </span>
           </div>
           <div className="itemContainer">
             <Mail className="icon" />
-            <span>safak@genius.com</span>
+            <span className="data">ahmedibrahimhassan654@gmail.com</span>
+          </div>
+          <div className="itemContainer">
+            <HomeIcon className="icon" />
+            <span className="data">Egypt </span>
           </div>
         </div>
 
-        <div className="right">this is right</div>
+        <div className="right">
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
