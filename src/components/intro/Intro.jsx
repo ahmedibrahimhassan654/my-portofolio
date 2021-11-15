14e11e48f491bc25fbf5176476df5231e3b4f49e
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-11-15 22:13:48 +0200

    finished intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,22 @@
 import "./intro.scss";
+import { init } from "ityped";
 import { useEffect, useRef } from "react";
 const Intro = () => {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: [
+        "Node Js  Developer",
+        "React Js Developer ",
+        "MERN Stack web Devloper ",
+      ],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -14,7 +30,7 @@ const Intro = () => {
           <h2>Hi There, I'm</h2>
           <h1>Ahmed Ibrahim</h1>
           <h3>
-            Freelance <span></span>
+            Freelance <span ref={textRef}></span>
           </h3>
         </div>
         <a href="#portofolio">
