718ca146f47df0e6eed4354537ff950d054a0a05
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-11-17 16:11:01 +0200

    portfolio section

--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -1,7 +1,72 @@
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portofolio.scss";
-
+import { MERNPortfolio, ReactJs, NodeJsBackEnd } from "../../data";
 const Portofolio = () => {
-  return <div className="Portofolio" id="portofolio"></div>;
+  const [selected, setSelected] = useState("Mern App");
+  const [data, setData] = useState([]);
+
+  const list = [
+    {
+      id: "Mern App",
+      title: "MERN App",
+    },
+    {
+      id: "React Js App",
+      title: "React Js App",
+    },
+    {
+      id: "Node js App",
+      title: "Node js App",
+    },
+  ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "Mern App":
+        setData(MERNPortfolio);
+        break;
+      case "React Js App":
+        setData(ReactJs);
+        break;
+      case "Node js App":
+        setData(NodeJsBackEnd);
+        break;
+
+      default:
+        setData(MERNPortfolio);
+    }
+  }, [selected]);
+  return (
+    <div className="Portofolio" id="portofolio">
+      <h1>Portofolio</h1>
+
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        {console.log(data)}
+        {data.map((d) => (
+          <div className="item">
+            <img src={d.img} alt="" />
+            <h3>{d.title}</h3>
+            <a href={d.github}>Github </a>
+
+            <div className="tec">
+              <p>{d.skills}</p> 
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Portofolio;
---
+++ src/data.js
@@ -0,0 +1,116 @@
+export const MERNPortfolio = [
+  {
+    id: 1,
+    title: "netflix-mern-app",
+    img: "https://images.ctfassets.net/4cd45et68cgf/2Dw84LjrNM9RE1zEeYTsU5/ee1dfa7815113172a82f88936ee4a513/______-___-netflix_-_______-________-_______.jpg?w=2560",
+    github: "https://github.com/ahmedibrahimhassan654/netflix-mern-app",
+    skills: ["node -", " react-", "bootstrab- ", "mongo- ", "express "],
+  },
+  {
+    id: 2,
+    title: "online learning app (private-repo)",
+    img: "https://w7.pngwing.com/pngs/27/972/png-transparent-pedagogy-massive-open-online-course-education-university-student-student.png",
+    github: "https://github.com/ahmedibrahimhassan654/Elearning-App",
+    skills: [
+      "node - ",
+      " react - ",
+      "next - ",
+      "bootstrab -",
+      " mongo - ",
+      " express - ",
+    ],
+  },
+  {
+    id: 3,
+    title: "E-commerce Web Design",
+    img: "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+    github: "https://github.com/ahmedibrahimhassan654/carCare",
+    skills: [
+      "node -",
+      "react -",
+      "bootstrab -",
+      "mongo -",
+      "express -",
+      "firebase -",
+    ],
+  },
+  {
+    id: 4,
+    title: "VIDEO-TALKER",
+    img: "https://images.financialexpress.com/2020/05/Video-call.jpg",
+    github: "https://github.com/ahmedibrahimhassan654/VIDEO-TALKER",
+    skills: ["node -", "react -", "socket.io -", " webRTC -"],
+  },
+
+  {
+    id: 5,
+    title: "Quize-App",
+    img: "https://propiece.s3-ap-northeast-1.amazonaws.com/product_image/20200729040504-00001759-quizwebappmadejavascripthtmlandcss-cropped.png",
+    github: "https://github.com/ahmedibrahimhassan654/quize-app",
+    skills: ["node -", " react -", "express-", "mongo -"],
+  },
+];
+
+export const NodeJsBackEnd = [
+  {
+    id: 1,
+    title: "luvina back-end api",
+    img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+    github: "https://github.com/ahmedibrahimhassan654/luvina-back-end-api",
+    skills: ["node -", "react - ", "express -", "mongo -"],
+  },
+  {
+    id: 2,
+    title: "CHAT-APP-API",
+    img: "https://cdn.dribbble.com/users/3967258/screenshots/15463803/media/4fddb9a2caf3b3bd634060f706a91e73.png?compress=1&resize=1200x900",
+    github: "https://github.com/ahmedibrahimhassan654/CHAT-APP-API",
+    skills: ["node -", "express -", "socket.io -"],
+  },
+  {
+    id: 3,
+    title: "Dev Code Camp",
+    img: "https://cdn.dribbble.com/users/3637068/screenshots/15467010/media/69a3279182b75365013fe285733d1122.png?compress=1&resize=1200x900",
+    github:
+      "https://github.com/ahmedibrahimhassan654/back-End-Development-project",
+    skills: ["node -", "react -", "express -", "mongo -"],
+  },
+];
+
+export const ReactJs = [
+  {
+    id: 1,
+    title: "Design Social Media App",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    skills: ["react "],
+  },
+  {
+    id: 2,
+    title: "Design Rampa UI Design",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    skills: ["react "],
+  },
+  {
+    id: 3,
+    title: "Design E-commerce Web Design",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    skills: ["react "],
+  },
+  {
+    id: 4,
+    title: "Design Relax Mobile App",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    skills: ["react "],
+  },
+  {
+    id: 5,
+    title: "Design Keser Web Design",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    skills: ["react "],
+  },
+  {
+    id: 6,
+    title: "Design Banking App",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    skills: ["react "],
+  },
+];
